64f5601f0f6f289f84dbfda8aee6119438d6572d
Monarch <monarchk7@gmail.com>
2021-08-14 11:01:24 +0530

    Initial commit

---
+++ Struct.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <string>
+using namespace std;
+
+int initial()
+{
+
+    std::string integer = {"123456789"};
+    std::string special = {"!@#$%^&*()"};
+    std::string uppercasealpha = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    std::string lowercasealpha = {"abcdefghijklmnopqrstuvwxyz"};
+    int singledigit;
+    srand(time(NULL));
+    singledigit = rand() % 10;
+    cout << integer.at(singledigit) << endl;
+    cout << special.at(singledigit) << endl;
+    cout << uppercasealpha.at(singledigit) << endl;
+    cout << lowercasealpha.at(singledigit) << endl;
+    cout<<singledigit<<endl;
+
+
+//random number in a range
+    int usinglealpha = (rand() %
+           (26 - 1 + 1)) + 1;
+
+           cout<<usinglealpha<<endl;
+
+    int lsinglealpha = (rand() %
+           (26 - 1 + 1)) + 1;
+
+           cout<<lsinglealpha<<endl;
+    // string thread = to_string(hash);
+
+    // cout << hash;
+    return 0;
+}
+
+int main()
+{
+    initial();
+
+    // srand(time(0));
+
+    // for (int i = 0; i < 4; i++)
+    //     cout << rand() << endl;
+    // printf(" %d ", rand());
+
+    return 0;
+}
